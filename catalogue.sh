5d05d42860e5d544756bf0bc35fff82877964870
Chimtamreddy <kr7348202@gmail.com>
2024-08-01 19:23:38 +0530

    roboshop-shellscript

--- catalogue.sh
+++ catalogue.sh
@@ -11,7 +11,7 @@ unzip /tmp/catalogue.zip
 cd /app
 npm install
 dnf install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+mongo --host 172.31.89.76 </app/schema/catalogue.js
 
 systemctl daemon-reload
 
