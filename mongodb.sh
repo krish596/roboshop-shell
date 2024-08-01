cb8f3d16fcc159c5aa6951ad0fddbef21393a619
Chimtamreddy <kr7348202@gmail.com>
2024-08-01 19:16:46 +0530

    roboshop-shellscript

--- mongodb.sh
+++ mongodb.sh
@@ -2,4 +2,4 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
