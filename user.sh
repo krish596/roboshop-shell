4e4c1a767dd683ea387c9b2cf8396180316d5e3b
Chimtamreddy <kr7348202@gmail.com>
2024-08-03 10:10:32 +0530

    roboshop-shellscript

--- user.sh
+++ user.sh
@@ -1,18 +1,32 @@
-cp user.service /etc/systemd/system/user.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-dnf install nodejs -y
-useradd roboshop
-mkdir /app
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+log=/tmp/roboshop.log
+echo -e "\e[32m>>>>>>>>>>>>>Download user Service File<<<<<<<<<<<<<<<\e[0m"
+cp user.service /etc/systemd/system/user.service &>>${log}
+echo -e "\e[32m>>>>>>>>>>>>>Download Mongo Repo file<<<<<<<<<<<<<<<\e[0m"
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+echo -e "\e[32m>>>>>>>>>>>>>Disable NodeJS<<<<<<<<<<<<<<<\e[0m"
+dnf module disable nodejs -y &>>${log}
+echo -e "\e[32m>>>>>>>>>>>>>Enable NodeJS<<<<<<<<<<<<<<<\e[0m"
+dnf module enable nodejs:18 -y &>>${log}
+echo -e "\e[32m>>>>>>>>>>>>>Install NodeJS<<<<<<<<<<<<<<<\e[0m"
+dnf install nodejs -y &>>${log}
+echo -e "\e[32m>>>>>>>>>>>>>Create Application User<<<<<<<<<<<<<<<\e[0m"
+useradd roboshop &>>${log}
+echo -e "\e[32m>>>>>>>>>>>>>Remove Application Directory<<<<<<<<<<<<<<<\e[0m"
+rm -rf /app &>>${log}
+echo -e "\e[32m>>>>>>>>>>>>>Create Application Directory<<<<<<<<<<<<<<<\e[0m"
+mkdir /app &>>${log}
+echo -e "\e[32m>>>>>>>>>>>>>Download Application Content<<<<<<<<<<<<<<<\e[0m"
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>${log}
 cd /app
-unzip /tmp/user.zip
-npm install
-dnf install mongodb-org-shell -y
-mongo --host mongodb.kr7348202.online </app/schema/user.js
-
-systemctl daemon-reload
-
-systemctl enable user
-systemctl restart user
+echo -e "\e[32m>>>>>>>>>>>>>Extract Content<<<<<<<<<<<<<<<\e[0m"
+unzip /tmp/user.zip &>>${log}
+echo -e "\e[32m>>>>>>>>>>>>>Install NPM<<<<<<<<<<<<<<<\e[0m"
+npm install &>>${log}
+echo -e "\e[32m>>>>>>>>>>>>>Install Mongo Client<<<<<<<<<<<<<<<\e[0m"
+dnf install mongodb-org-shell -y &>>${log}
+echo -e "\e[32m>>>>>>>>>>>>>Load Mongo Schema<<<<<<<<<<<<<<<\e[0m"
+mongo --host mongodb.kr7348202.online </app/schema/user.js &>>${log}
+echo -e "\e[32m>>>>>>>>>>>>>Start user Service<<<<<<<<<<<<<<<\e[0m"
+systemctl daemon-reload &>>${log} | tee -a ${log}
+systemctl enable user &>>${log} | tee -a ${log}
+systemctl restart user &>>${log} | tee -a ${log}
