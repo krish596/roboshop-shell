cdc6c0420927cace9a91fb8abe6455712e8ddeae
Chimtamreddy <kr7348202@gmail.com>
2024-08-03 14:35:29 +0530

    roboshop-shellscript

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,21 @@
-dnf install nginx -y
-cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+source common.sh
+echo -e "\e[32m>>>>>>>>>>>>>Install Nginx<<<<<<<<<<<<<<<\e[0m"
+dnf install nginx -y &>>${log}
+func_exit_status
+echo -e "\e[32m>>>>>>>>>>>>>Create Roboshop Conf file<<<<<<<<<<<<<<<\e[0m"
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
+func_exit_status
+echo -e "\e[32m>>>>>>>>>>>>>Remove Existing Content<<<<<<<<<<<<<<<\e[0m"
+rm -rf /usr/share/nginx/html/* &>>${log}
+func_exit_status
+echo -e "\e[32m>>>>>>>>>>>>>Download Application Content<<<<<<<<<<<<<<<\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
+func_exit_status
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl restart nginx
+echo -e "\e[32m>>>>>>>>>>>>>Extract Content<<<<<<<<<<<<<<<\e[0m"
+unzip /tmp/frontend.zip &>>${log}
+func_exit_status
+echo -e "\e[32m>>>>>>>>>>>>>Start frontend service<<<<<<<<<<<<<<<\e[0m"
+systemctl enable nginx &>>${log}
+systemctl restart nginx &>>${log}
+func_exit_status
