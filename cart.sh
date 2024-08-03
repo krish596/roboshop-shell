54a04feb5cd1bc9590d6717cb41a206c4bea87ba
Chimtamreddy <kr7348202@gmail.com>
2024-08-03 10:18:04 +0530

    roboshop-shellscript

--- cart.sh
+++ cart.sh
@@ -1,6 +1,7 @@
 log=/tmp/roboshop.log
 echo -e "\e[32m>>>>>>>>>>>>>Download cart Service File<<<<<<<<<<<<<<<\e[0m"
 cp cart.service /etc/systemd/system/cart.service &>>${log}
+
 echo -e "\e[32m>>>>>>>>>>>>>Disable NodeJS<<<<<<<<<<<<<<<\e[0m"
 dnf module disable nodejs -y &>>${log}
 echo -e "\e[32m>>>>>>>>>>>>>Enable NodeJS<<<<<<<<<<<<<<<\e[0m"
@@ -20,6 +21,8 @@ echo -e "\e[32m>>>>>>>>>>>>>Extract Content<<<<<<<<<<<<<<<\e[0m"
 unzip /tmp/cart.zip &>>${log}
 echo -e "\e[32m>>>>>>>>>>>>>Install NPM<<<<<<<<<<<<<<<\e[0m"
 npm install &>>${log}
+
+echo -e "\e[32m>>>>>>>>>>>>>Start cart Service<<<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload &>>${log} | tee -a ${log}
 systemctl enable cart &>>${log} | tee -a ${log}
 systemctl restart cart &>>${log} | tee -a ${log}
