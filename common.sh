4cf4b9cfef274bf8e846baada9517f8d05b92ae6
Chimtamreddy <kr7348202@gmail.com>
2024-08-03 14:28:10 +0530

    roboshop-shellscript

--- common.sh
+++ common.sh
@@ -2,7 +2,7 @@ log=/tmp/roboshop.log
 
 func_exit_status() {
   if [ $? -eq 0 ]; then
-    echo -e "\e[31mSUCCESS\e[0m"
+    echo -e "\e[34mSUCCESS\e[0m"
   else
     echo -e "\e[32mFAILURE\e[0m"
   fi 
